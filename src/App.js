6eb7c4195391bc1e437b17e9d9cc8e6cdb73fcbd
oliverjam <oliverjamesphillips@gmail.com>
2018-10-04 09:06:36 +0100

    Initial set up
    All drink data and dependencies, plus basic UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Downshift from 'downshift';
+import matchSorter from 'match-sorter';
+import { Flex, Box, Heading, Text } from 'rebass';
+// import { ThemeProvider } from 'emotion-theming';
 
 class App extends Component {
+  componentDidMount() {
+    import('./drinks').then(drinks =>
+      this.setState({ drinks: drinks.default })
+    );
+    import('./ingredients').then(ingredients =>
+      this.setState({ ingredients: ingredients.default })
+    );
+  }
+  state = {
+    drinks: [],
+    ingredients: [],
+  };
   render() {
+    const { drinks, ingredients } = this.state;
+    if (!drinks.length || !ingredients.length) return <div>Loading...</div>;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Flex>
+        <Box width={1 / 2}>
+          <Heading as="h3">Ingredients</Heading>
+          <Downshift>
+            {({
+              getInputProps,
+              getItemProps,
+              isOpen,
+              inputValue,
+              highlightedIndex,
+            }) => (
+              <div>
+                <Text is="label" htmlFor="ingredient-search">
+                  Add ingredients
+                </Text>
+                <input {...getInputProps({ id: 'ingredient-search' })} />
+                {isOpen && (
+                  <Box as="ul">
+                    {matchSorter(ingredients, inputValue).map(
+                      (ingredient, i) => (
+                        <Text
+                          {...getItemProps({ item: ingredient })}
+                          as="li"
+                          key={ingredient}
+                          p={1}
+                          bg={highlightedIndex === i ? '#ccc' : 'transparent'}
+                        >
+                          {ingredient}
+                        </Text>
+                      )
+                    )}
+                  </Box>
+                )}
+              </div>
+            )}
+          </Downshift>
+        </Box>
+        <Box width={1 / 2}>
+          <Heading as="h3">Drinks</Heading>
+          <Box as="ul">
+            {drinks.slice(0, 20).map(drink => (
+              <Text as="li" key={drink.idDrink}>
+                {drink.strDrink}
+              </Text>
+            ))}
+          </Box>
+        </Box>
+      </Flex>
     );
   }
 }
