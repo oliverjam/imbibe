78eb4df7a7fa6b2b21dd7c002cd71e4b30226f1c
oliverjam <oliverjamesphillips@gmail.com>
2018-10-05 10:09:28 +0100

    Select ingredients and filter drinks

--- src/App.js
+++ src/App.js
@@ -16,35 +16,51 @@ class App extends Component {
   state = {
     drinks: [],
     ingredients: [],
+    myIngredients: [],
+  };
+  selectIngredient = (newIng, { reset }) => {
+    this.setState(prev => ({ myIngredients: [...prev.myIngredients, newIng] }));
+    reset({ inputValue: '' });
   };
   render() {
-    const { drinks, ingredients } = this.state;
+    const { drinks, ingredients, myIngredients } = this.state;
     if (!drinks.length || !ingredients.length) return <div>Loading...</div>;
     return (
       <Flex>
         <Box width={1 / 2}>
           <Heading as="h3">Ingredients</Heading>
-          <Downshift>
+          <Downshift
+            onSelect={this.selectIngredient}
+            // stateReducer={(state, changes) =>
+            //   console.log({ state, changes }) || changes
+            // }
+          >
             {({
               getInputProps,
+              getMenuProps,
               getItemProps,
               isOpen,
               inputValue,
               highlightedIndex,
             }) => (
-              <div>
+              <div style={{ position: 'relative' }}>
                 <Text is="label" htmlFor="ingredient-search">
                   Add ingredients
                 </Text>
                 <input {...getInputProps({ id: 'ingredient-search' })} />
                 {isOpen && (
-                  <Box as="ul">
+                  <Box
+                    as="ul"
+                    bg="white"
+                    css={{ position: 'absolute', top: 16 }}
+                    {...getMenuProps()}
+                  >
                     {matchSorter(ingredients, inputValue).map(
                       (ingredient, i) => (
                         <Text
                           {...getItemProps({ item: ingredient })}
                           as="li"
-                          key={ingredient}
+                          key={`searchIng- ${ingredient}`}
                           p={1}
                           bg={highlightedIndex === i ? '#ccc' : 'transparent'}
                         >
@@ -57,15 +73,27 @@ class App extends Component {
               </div>
             )}
           </Downshift>
+          {myIngredients.map(ing => (
+            <Text as="li" key={`myIng-${ing}`}>
+              {ing}
+            </Text>
+          ))}
         </Box>
         <Box width={1 / 2}>
           <Heading as="h3">Drinks</Heading>
           <Box as="ul">
-            {drinks.slice(0, 20).map(drink => (
-              <Text as="li" key={drink.idDrink}>
-                {drink.strDrink}
-              </Text>
-            ))}
+            {drinks
+              .slice(0, 1)
+              .filter(drink =>
+                drink.ingredients.every(
+                  ing => console.log(ing) || myIngredients.includes(ing)
+                )
+              )
+              .map(drink => (
+                <Text as="li" key={drink.idDrink}>
+                  {drink.strDrink}
+                </Text>
+              ))}
           </Box>
         </Box>
       </Flex>
