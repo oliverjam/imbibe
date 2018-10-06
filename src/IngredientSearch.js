c27bf956b61a6f948b636447c2724b79ade3dd34
oliverjam <oliverjamesphillips@gmail.com>
2018-10-06 13:13:59 +0100

    Style ingredient search bar

--- src/IngredientSearch.js
+++ src/IngredientSearch.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import Downshift from 'downshift';
 import matchSorter from 'match-sorter';
-import { Box, Text } from 'rebass';
+import styled from 'styled-components';
+import { Text } from 'rebass';
+
+const shadow = '0 1px 3px hsla(200, 20%, 20%, 0.5)';
+
+const Label = styled.label`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const SearchInput = styled.input.attrs({ type: 'text' })`
+  width: 100%;
+  margin-top: 0.25rem;
+  padding: 0.5rem 1rem;
+  background-color: hsl(200, 10%, 90%);
+  &:focus {
+    box-shadow: ${shadow};
+  }
+`;
+
+const SearchResults = styled.ul`
+  position: relative;
+  top: 1rem;
+  background-color: white;
+  box-shadow: ${shadow};
+`;
 
 const IngredientSearch = ({ selectIngredient, ingredients }) => (
   <Downshift onSelect={selectIngredient}>
@@ -13,30 +38,31 @@ const IngredientSearch = ({ selectIngredient, ingredients }) => (
       inputValue,
       highlightedIndex,
     }) => (
-      <div style={{ position: 'relative' }}>
-        <Text is="label" htmlFor="ingredient-search">
-          Add ingredients
-        </Text>
-        <input {...getInputProps({ id: 'ingredient-search' })} />
+      <div style={{ position: 'relative', marginTop: 8 }}>
+        <Label htmlFor="ingredient-search">Add ingredients</Label>
+        <SearchInput
+          {...getInputProps({
+            id: 'ingredient-search',
+            placeholder: 'Bourbon...',
+          })}
+        />
         {isOpen && (
-          <Box
-            as="ul"
-            bg="white"
-            css={{ position: 'absolute', top: 16 }}
-            {...getMenuProps()}
-          >
+          <SearchResults {...getMenuProps()}>
             {matchSorter(ingredients, inputValue).map((ingredient, i) => (
               <Text
                 {...getItemProps({ item: ingredient })}
                 as="li"
                 key={`searchIng- ${ingredient}`}
-                p={1}
-                bg={highlightedIndex === i ? '#ccc' : 'transparent'}
+                px={3}
+                py={2}
+                bg={
+                  highlightedIndex === i ? 'hsl(200, 10%, 90%)' : 'transparent'
+                }
               >
                 {ingredient}
               </Text>
             ))}
-          </Box>
+          </SearchResults>
         )}
       </div>
     )}
