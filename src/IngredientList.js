cde3da1efc9ddff704b62a20583e5993a64dad2c
oliverjam <oliverjamesphillips@gmail.com>
2020-02-09 12:16:54 +0000

    Add copy ingredients to clipboard button

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -1,39 +1,54 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
-import posed, { PoseGroup } from 'react-pose';
-import IngredientSearch from './IngredientSearch';
-import { BorderBox, IconButton } from './css';
-import { Bottle } from './icons';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import posed, { PoseGroup } from "react-pose";
+import IngredientSearch from "./IngredientSearch";
+import { BorderBox, IconButton, border } from "./css";
+import { Bottle } from "./icons";
 
 const IngredientList = ({
   myIngredients,
   selectIngredient,
   removeIngredient,
-}) => (
-  <Fragment>
-    <h1>
-      <Bottle /> My Ingredients
-    </h1>
-    <IngredientSearch selectIngredient={selectIngredient} />
-    <List>
-      <PoseGroup>
-        {myIngredients.map(ing => (
-          <ListItem key={`myIng-${ing}`}>
-            <BorderBox>
-              <Content>{ing[0].toUpperCase() + ing.slice(1)}</Content>
-              <IconButton
-                onClick={removeIngredient(ing)}
-                aria-label={`Remove ${ing}`}
-              >
-                &times;
-              </IconButton>
-            </BorderBox>
-          </ListItem>
-        ))}
-      </PoseGroup>
-    </List>
-  </Fragment>
-);
+}) => {
+  const shareIngredients = () => {
+    const encoded = myIngredients.join(",");
+    const url = new URL(window.location);
+    url.search = `ingredients=${encoded}`;
+    navigator.clipboard
+      .writeText(url.toString())
+      .then(() => alert("Copied shareable URL to clipboard"))
+      .catch(console.error);
+  };
+  return (
+    <Fragment>
+      <Header>
+        <h1>
+          <Bottle /> My Ingredients
+        </h1>
+        <Button onClick={shareIngredients}>Share ingredients</Button>
+      </Header>
+      <IngredientSearch selectIngredient={selectIngredient} />
+
+      <List>
+        <PoseGroup>
+          {myIngredients.map(ing => (
+            <ListItem key={`myIng-${ing}`}>
+              <BorderBox>
+                <Content>{ing[0].toUpperCase() + ing.slice(1)}</Content>
+                <IconButton
+                  onClick={removeIngredient(ing)}
+                  aria-label={`Remove ${ing}`}
+                >
+                  &times;
+                </IconButton>
+              </BorderBox>
+            </ListItem>
+          ))}
+        </PoseGroup>
+      </List>
+    </Fragment>
+  );
+};
 
 const List = styled.ul`
   margin-top: 1rem;
@@ -54,6 +69,19 @@ const Content = styled.div`
   align-items: center;
   padding: 0.5rem 1rem;
   background-color: #fff;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Button = styled.button`
+  border: ${border};
+  padding: 0.5rem;
+  color: white;
+  font: inherit;
+  background-color: hsl(var(--hue), 50%, 30%);
 `;
 
 export default IngredientList;
